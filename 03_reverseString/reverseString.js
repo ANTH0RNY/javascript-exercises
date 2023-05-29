c4237e8686a230dd9270ab9e477e5bbfa19985f8
ANTH0RNY <nyaganthony@gmail.com>
2023-05-29 14:10:03 +0300

    Done with 03_rerverseString All test passed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    const arr = str.split('').reverse();
+    let empty = '';
+    for (let i = 0; i < arr.length; i++){
+        empty += arr[i]
+    }
+    return empty
 };
 
 // Do not edit below this line
