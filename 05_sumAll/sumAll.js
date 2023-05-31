b986b12145c47a648ada4d84dab6e5e26ed3948c
ANTH0RNY <nyaganthony@gmail.com>
2023-05-31 12:38:59 +0300

    Done with 05_sumAll all tests passed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    let final = 0;
+    if (start < 0 || end < 0 || typeof start != 'number' || typeof end != 'number'){
+        return 'ERROR'
+    }
+    if (start < end){
+        for (let i = start; i <= end;i++){
+            final += i
+        }
+    }
+    else{
+        for (let i = end; i <= start;i++){
+            final += i
+        }
+    }
+    return final
 };
 
 // Do not edit below this line
