57206296aa928bfb771e91886d484027face9f22
ANTH0RNY <nyaganthony@gmail.com>
2023-05-31 14:45:22 +0300

    Done with 07_tempConversion All tests passed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  const cel = (temp - 32) * (5/9)
+  return Number(cel.toFixed(1))
+  
 };
+const convertToFahrenheit = function(temp) {
+  const fahnr = (temp * (9/5)) + 32
+  return Number(fahnr.toFixed(1))
 
-const convertToFahrenheit = function() {
 };
 
 // Do not edit below this line
