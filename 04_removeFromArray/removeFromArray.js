b0d8b17eb3e8fd73643759c0bcbf09d9181f8ffd
ANTH0RNY <nyaganthony@gmail.com>
2023-05-31 12:14:36 +0300

    Done with 04_removeFromArray All test is passed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,20 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...args) {
+    function remove(arr, int){
+      const len = arr.length;
+      const index = arr.indexOf(int)
+      if (index === -1){
+        return arr;
+      }
+      let result = []
+      result = result.concat(arr.slice(0,index))
+      result = result.concat(arr.slice(index+1, len))
+      return result
+     }
+     let result = [...arr]
+     for (let i of args){
+        result = [...remove(result, i)];
+     }
+     return result
 };
 
 // Do not edit below this line
